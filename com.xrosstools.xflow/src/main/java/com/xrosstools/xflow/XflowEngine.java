a4658ef13491440fce1b6802d40b7e3c01235eca
He, Jiehui <he_jiehui@163.com>
2025-08-11 15:47:26 +0800

    release 1.0.6
    add XflowEngine shutdown hook

--- com.xrosstools.xflow/src/main/java/com/xrosstools/xflow/XflowEngine.java
+++ com.xrosstools.xflow/src/main/java/com/xrosstools/xflow/XflowEngine.java
@@ -7,8 +7,37 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class XflowEngine {
-	private static final ExecutorService executor = Executors.newCachedThreadPool();
-	private static final ScheduledExecutorService waitExecutor = Executors.newScheduledThreadPool(Runtime.getRuntime().availableProcessors());
+	private static ExecutorService executor;
+	private static ScheduledExecutorService waitExecutor;
+
+	static {
+		executor = Executors.newCachedThreadPool();
+		waitExecutor = Executors.newScheduledThreadPool(Runtime.getRuntime().availableProcessors());
+		
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+            	shutdown();
+            }
+        }));
+	}
+	
+	public static void shutdown() {
+		System.out.println("Shutdown XflowEngine");
+        shutdown(executor);
+        shutdown(waitExecutor);
+	}
+
+	private static void shutdown(ExecutorService service) {
+		service.shutdown();
+        try {
+            if (!service.awaitTermination(10, TimeUnit.SECONDS)) {
+            	service.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+        	service.shutdownNow();
+        }            
+	}
 
 	public static void submit(XflowContext context, Node node) {
 		submit(new ActiveToken(context, node));
